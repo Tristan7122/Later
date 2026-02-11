ef9d599f2c9fdc83e53966108b50ca384142bfca
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2026-02-11 14:58:40 +0200

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  BOT_NAME: "PHORA-MDx",
+  OWNER_NAME: "Tristan",
+  OWNER_NUMBER: ["27634624586"],
+  PREFIX: ".",
+  MODE: "private",
+  AUTO_REACT: true,
+  AUTO_REPLY_THANKS: true,
+  PORT: 3000
+}
